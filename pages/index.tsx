af3a72ef763cf9f0f4362ba5879d2fd7744aac48
geritol <gergo.tolnai@gmail.com>
2022-01-04 16:13:02 +0100

    chore: fix lint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Generate from "../frontend/components/Generate";
 
 function HomePage() {
@@ -11,21 +12,25 @@ function HomePage() {
           <a href="https://github.com/geritol/match-tag-to-package-version">
             geritol/match-tag-to-package-version
           </a>{" "}
-          <a href="/geritol/match-tag-to-package-version">
-            <img
-              alt="/geritol/match-tag-to-package-version"
-              src="https://img.shields.io/endpoint?url=https%3A%2F%2Factions.geritol.tech%2Fapi%2Fgeritol%2Fmatch-tag-to-package-version2%3Fformat%3Dshields.io"
-            />
-          </a>
+          <Link href="/geritol/match-tag-to-package-version">
+            <a>
+              <img
+                alt="/geritol/match-tag-to-package-version"
+                src="https://img.shields.io/endpoint?url=https%3A%2F%2Factions.geritol.tech%2Fapi%2Fgeritol%2Fmatch-tag-to-package-version2%3Fformat%3Dshields.io"
+              />
+            </a>
+          </Link>
         </li>
         <li>
           <a href="https://github.com/actions/checkout">actions/checkout</a>{" "}
-          <a href="actions/checkout">
-            <img
-              alt="actions/checkout user count"
-              src="https://img.shields.io/endpoint?url=https%3A%2F%2Factions.geritol.tech%2Fapi%2Factions%2Fcheckout2%3Fformat%3Dshields.io"
-            />
-          </a>
+          <Link href="actions/checkout">
+            <a>
+              <img
+                alt="actions/checkout user count"
+                src="https://img.shields.io/endpoint?url=https%3A%2F%2Factions.geritol.tech%2Fapi%2Factions%2Fcheckout2%3Fformat%3Dshields.io"
+              />
+            </a>
+          </Link>
         </li>
       </ul>
 
