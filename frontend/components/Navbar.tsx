6a00cd795276efaab39b64a84e2e4f4f3a97c6cb
geritol <gergo.tolnai@gmail.com>
2022-01-04 23:29:41 +0100

    style: add minimal padding on mobile

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 function Navbar() {
   return (
     <header className="pt-4">
-      <div className="max-w-5xl mx-auto flex justify-between content-center">
+      <div className="max-w-5xl mx-auto flex justify-between content-center px-2 md:px-1">
         <h1 className="text-lg">
           <Link href="/">
             <a className="text-stone-800 font-semibold">GHA users badges</a>
