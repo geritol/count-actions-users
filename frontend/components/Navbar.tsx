125b49dc6814c029fc5a2630671bc8f5d65f4872
geritol <gergo.tolnai@gmail.com>
2022-01-04 23:15:27 +0100

    feat(nav): add contribute on github

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -2,13 +2,18 @@ import Link from "next/link";
 
 function Navbar() {
   return (
-    <header className="pt-3">
-      <div className="max-w-5xl mx-auto">
+    <header className="pt-4">
+      <div className="max-w-5xl mx-auto flex justify-between content-center">
         <h1 className="text-lg">
           <Link href="/">
             <a className="text-stone-800 font-semibold">GHA users badges</a>
           </Link>
         </h1>
+
+        <div>
+          Contribute on{" "}
+          <a href="https://github.com/geritol/count-actions-users">Github</a>
+        </div>
       </div>
     </header>
   );
