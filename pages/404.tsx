7d34e1cae9c572b7f3b3502e6dbc41770edb8b4e
geritol <gergo.tolnai@gmail.com>
2022-01-04 23:02:58 +0100

    feat: 404 on invalid params

---
+++ pages/404.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../frontend/components/Navbar";
+
+export default function Custom404() {
+  return (
+    <div>
+      <Navbar />
+      <main>
+        <h1>404 - Page Not Found</h1>
+      </main>
+    </div>
+  );
+}
--- pages/[user]/[repo].tsx
+++ pages/[user]/[repo].tsx
@@ -2,10 +2,16 @@ import { useRouter } from "next/router";
 import Navbar from "../../frontend/components/Navbar";
 import Shield from "../../frontend/components/Shield";
 import { getRepoUrl } from "../../frontend/urls";
+import Custom404 from "../404";
 
 function ActionStats() {
   const router = useRouter();
   const { user, repo } = router.query;
+
+  if (typeof user !== "string" || typeof repo !== "string") {
+    return <Custom404 />;
+  }
+
   return (
     <div>
       <Navbar />
