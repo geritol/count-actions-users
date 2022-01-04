c994a68f4cebbea4b0231cf087c606dba83797c9
geritol <gergo.tolnai@gmail.com>
2022-01-04 21:48:30 +0100

    feat(generate): debounce input

--- frontend/components/Generate.tsx
+++ frontend/components/Generate.tsx
@@ -1,14 +1,25 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { getActionUrl, getShieldUrl } from "../urls";
+import debounce from "lodash.debounce";
 
 function Generate() {
   const [value, setValue] = useState("actions/checkout");
   const [isValid, setIsValid] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const validate = useCallback(
+    debounce((value) => {
+      const isValid = value.split("/").length == 2;
+      setIsValid(isValid);
+      setIsLoading(false);
+    }, 500),
+    []
+  );
+
   const handleChange = (e) => {
     const value = e.target.value.trim();
+    setIsLoading(true);
     setValue(value);
-    const isValid = value.split("/").length == 2;
-    setIsValid(isValid);
+    validate(value);
   };
 
   const [user, repo] = value.split("/");
@@ -19,10 +30,11 @@ function Generate() {
     <div>
       <h2>Generate badge</h2>
       <input value={value} onChange={handleChange}></input>
-      {!isValid && (
+      {!isValid && !isLoading && (
         <div>Please provide Github Action in `owner/repo` format</div>
       )}
-      {isValid && (
+      {isLoading && <div>Loading...</div>}
+      {isValid && !isLoading && (
         <div>
           <a href={actionUrl}>
             <img alt={`${value} user count`} src={shieldUrl} />
