59089853f33765c071a600a145267ebe82ed2f86
geritol <gergo.tolnai@gmail.com>
2022-01-04 17:16:47 +0100

    fix: example shields

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ function HomePage() {
             <a>
               <img
                 alt="/geritol/match-tag-to-package-version"
-                src="https://img.shields.io/endpoint?url=https%3A%2F%2Factions.geritol.tech%2Fapi%2Fgeritol%2Fmatch-tag-to-package-version2%3Fformat%3Dshields.io"
+                src="https://img.shields.io/endpoint?url=https%3A%2F%2Factions.geritol.tech%2Fapi%2Fgeritol%2Fmatch-tag-to-package-version%3Fformat%3Dshields.io"
               />
             </a>
           </Link>
@@ -27,7 +27,7 @@ function HomePage() {
             <a>
               <img
                 alt="actions/checkout user count"
-                src="https://img.shields.io/endpoint?url=https%3A%2F%2Factions.geritol.tech%2Fapi%2Factions%2Fcheckout2%3Fformat%3Dshields.io"
+                src="https://img.shields.io/endpoint?url=https%3A%2F%2Factions.geritol.tech%2Fapi%2Factions%2Fcheckout%3Fformat%3Dshields.io"
               />
             </a>
           </Link>
